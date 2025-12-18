7a208dc02237a3278f0a2a42d39118147706f45e
user530 <m.timarsuev@gmail.com>
2025-12-19 00:24:16 +0300

    Added 2 base routes

--- students_app/views.py
+++ students_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def home(request):
+    return JsonResponse({
+    'title': 'Student Attitude & Behaviour API',
+    'api_version': '1.0',
+    'documentation_url': '<PENDING>',
+    'links': {
+        'self': {
+            'href': '/',
+            'methods': ['GET'],
+            'rel': 'self',
+        },
+        'health': {
+            'href': '/health',
+            'methods': ['GET'],
+            'rel': 'status',
+        },
+    },
+})
+
+def health(request):
+    return JsonResponse({
+        'service': 'Student Attitude & Behaviour API',
+        'api_version': '1.0',
+        'status': 'ok',
+    })
