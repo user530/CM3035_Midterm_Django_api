11cd13023167ee2a5f2b0429b152c260e9d84544
user530 <m.timarsuev@gmail.com>
2025-12-23 20:08:51 +0300

    Fixed missing models in admin (registered them)

--- students_app/admin.py
+++ students_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, Department, Hobby, StudentMetrics
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(StudentMetrics)
+admin.site.register(Department)
+admin.site.register(Hobby)
